7972fd35d693bc30582bff353a5b5fd69cfa58f7
SuicideG4M3R <suicideg4m3r@gmail.com>
2024-06-18 21:37:45 +0200

    changing location

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import './style.css'
+import '../public/assets/style/style.css';
 import AboutPage from './screens/about-page/about-page.js'
 import ProjectsPage from './screens/projects-page/projects-page.js'
 import LandingPage from './screens/landing-page/landing-page.js'
 import FunPage from './screens/fun-page/fun-page.js'
+
 import FunProjectPage from './screens/fun-project-page/fun-project-page.js' // heres project page
 import NotFound from './screens/not-found/not-found.js' // not found
 
